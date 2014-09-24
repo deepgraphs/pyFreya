d631b0368ff45b037222d0fe50892012795faafe
evankos <underworldseed@gmail.com>
2014-09-24 04:10:21 -0400

    new models

---
+++ pyFreya/freya/model/Ontology.py
@@ -0,0 +1,15 @@
+__author__ = 'root'
+# A placeholder of definitions until a python alternative to openrdf is selected
+class OWL(object):
+    DATATYPEPROPERTY="http://www.w3.org/2002/07/owl#DatatypeProperty"
+    OBJECTPROPERTY="http://www.w3.org/2002/07/owl#ObjectProperty"
+    CLASS="http://www.w3.org/2002/07/owl#Class"
+class RDFS(object):
+    SUBCLASSOF="http://www.w3.org/2000/01/rdf-schema#subClassOf"
+    DOMAIN="http://www.w3.org/2000/01/rdf-schema#domain"
+    CLASS="http://www.w3.org/2000/01/rdf-schema#Class"
+    PREFIX="rdfs"
+class RDF(object):
+    PROPERTY="http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"
+if __name__=='__main__':
+    print OWL.CLASS
